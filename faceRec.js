17b2fe37229dd5fea63225683362e0186a85fca9
Jason Leibel <Jason.Leibel@vecima.com>
2020-01-31 08:37:45 -0600

    init: commit

---
+++ faceRec.js
@@ -0,0 +1,43 @@
+// Import all required models
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('models')
+]).then(startVideo).catch(function (err) {
+    console.log(err.message); // some coding error in handling happened
+});
+
+const video = document.getElementById('camVideo');
+
+function startVideo() {
+    // Heard this was depricated? Still works though.
+    navigator.getUserMedia(
+        {video: {}},
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
+}
+
+video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+    const displaySize = {width: video.width, height: video.height};
+    faceapi.matchDimensions(canvas, displaySize);
+    setInterval(async () => {
+
+        const faceDetections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+        const resizedDetections = faceapi.resizeResults(faceDetections, displaySize);
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+
+        faceapi.draw.drawDetections(canvas, resizedDetections);
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+        faceDetections.get;
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+
+        // 80% positivity on being sad.
+        if (faceDetections !== null  && typeof(faceDetections[0]) !== "undefined" && faceDetections[0].expressions.sad > 0.8) {
+            console.log("Cheer up");
+        }
+    }, 100)
+});
